a4b56ec2b27df28ceecfee83e9e6525691c99272
Lucas Vizoto <127165501+LucasVizoto@users.noreply.github.com>
2026-04-11 13:47:57 +0000

    [FEAT]: Finalizing the implementation of the calculator language based on the information from the PDF file in the AVA

--- lexer.py
+++ lexer.py
@@ -1,106 +1,160 @@
-# Código-fonte em "calculator language"
-# para análise léxica
-source : str = """
+#Código-fonte em "calculator language" para análise léxica
+source: str = """
 read a
 read b
 read c
 result := (a + b) * c
 write result
-""""
+"""
 
-# Caracteres considerados "em branco"
+#Caracteres considerados "em branco"
 BLANKS = {
-    " ",    # Espaço em branco 
-    "\t",   # Tabulação
-    "\n",   # Quebra de linha
-    "\r"    # Retorno de linha
+    " ",    #Espaço em branco
+    "\t",   #Tabulação
+    "\n",   #Quebra de linha
+    "\r"    #Retorna de linha
 }
 
-# Funções auxiliares
+#funções auxiliares
 def is_alpha(c: str) -> bool:
     # Letras maiúsculas e minúsculas sem acento
     return c.isascii() and c.isalpha()
 
 def is_digit(c: str) -> bool:
-    # 0 a 9
+    # De 0 a 9
     return c.isascii() and c.isdigit()
 
 def is_alphanum(c: str) -> bool:
-    # Maiúsculas e minúsculas sem acento + dígitos
+    # Maiúsculas e minusculas sem acento + digitos
     return c.isascii() and c.isalnum()
 
-# Função que efetua a análise propriamente dita
+#Funcao que efetua a análise propriamente dita
 def analyze(source: str) -> None:
-
-    # Variáveis de controle
+    #Variáveis de controle
     state = 0
     lexeme = ""
     row = 1
     col = 1
     symbols_table = []
 
-    # Adiciona uma quebra de linha ao final do código-fonte para
-    # possibilitar o processamento do último lexema
+    #Adiciona uma quebra de linha ao final do código-fonte para possibilitar o processamento do último lexema
     source += "\n"
 
-    # Função que opera a mudança de estado
-    def go_to_state(ch: str, next_state: int) -> tuple[str, int]:
-        # Adiciona o ch atual ao lexema e move para o próximo estado
+    #Função que opera a mudança de estado
+    def go_to_state (ch: str, next_state:int) -> tuple[str,int]:
         return lexeme + ch, next_state
 
-    # Função que aceita um lexema válido e o insere na tabela de símbolos
+    #Função que aceita um lexema válido e o insere na tabela de símbolos
     def accept(ch: str, terminal: int) -> tuple[str, int]:
-        pass    # Vamos implementar isso depois
+        pass #Vamos Implementar isso depois
 
-    # Função que exibe um erro caso a análise léxica falhe
-    def display_error(ch: str) -> None:
-        pass    # Também vamos implementar isso depois
+    #Função que exibe um erro caso a análise léxica falhe
+    def display_error(ch: str, teminal: int) -> tuple[str,int]:
+        pass
 
-    pos = 0     # Posição atual de leitura dentro do código-fonte
+    pos = 0 #Posição atual de leitura dentro do código-fonte
     while pos < len(source):
-        ch = source[pos]        # Lê um caractere do código fonte
+        ch = source[pos]   #Lê um caracter
 
-        # Se for uma quebra de linha, ajusta a contagem de linhas e colunas
+        #Se for uma quebra de linha, ajuste a contagem de linhas e colunas
         if ch == "\n":
             row += 1
             col = 0
 
         match state:
-            case 0:         
-                if ch == "r":           lexeme, state = go_to_state(ch, 10)
-                elif ch == "w":         lexeme, state = go_to_state(ch, 60)
-                elif is_alpha(ch):      lexeme, state = go_to_state(ch, 50)
-                elif is_digit(ch):      lexeme, state = go_to_state(ch, 110)
-                elif ch == ".":         lexeme, state = go_to_state(ch, 130)
-                elif ch == ":":         lexeme, state = go_to_state(ch, 150)
-                elif ch == "+":         lexeme, state = accept(ch, 1006)
-                elif ch == "-":         lexeme, state = accept(ch, 1007)
-                elif ch == "*":         lexeme, state = accept(ch, 1008)
-                elif ch == "/":         lexeme, state = accept(ch, 1009)
-                elif ch == "(":         lexeme, state = accept(ch, 1010)
-                elif ch == ")":         lexeme, state = accept(ch, 1011)
-                elif ch in BLANKS:      pass        # Ignora
-                else:                   display_error(ch)
+            case 0: 
+                if ch == "r": lexeme, state = go_to_state(ch, 10) 
+                elif ch == "w": lexeme, state = go_to_state(ch, 60) 
+                elif is_alpha(ch): lexeme, state = go_to_state(ch, 50)
+                elif is_digit(ch): lexeme, state = go_to_state(ch, 110) 
+                elif ch == '.': lexeme, state = go_to_state(ch, 130) 
+                elif ch == ':': lexeme, state = go_to_state(ch, 150) 
+                elif ch == '+': lexeme, state = accept(ch, 1006) 
+                elif ch == '-': lexeme, state = accept(ch, 1007) 
+                elif ch == '*': lexeme, state = accept(ch, 1008) 
+                elif ch == '/': lexeme, state = accept(ch, 1009) 
+                elif ch == '(': lexeme, state = accept(ch, 1010) 
+                elif ch == ')': lexeme, state = accept(ch, 1011) 
+                elif ch in BLANKS: pass #ignora
+                else: display_error(ch)
 
             case 10:
-                if ch == "e":           lexeme, state = go_to_state(ch, 20)
-                elif is_alphanum(ch):   lexeme, state = go_to_state(ch, 50)
-                elif ch in BLANKS:      lexeme, state = accept(ch, 1001)
-                else                    display_error(ch)
+                if ch == 'e': lexeme, state = go_to_state(ch, 20)
+                elif is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+                else: display_error(ch)
 
             case 20:
-                if ch == "a":           lexeme, state = go_to_state(ch, 30)
-                elif is_alphanum(ch):   lexeme, state = go_to_state(ch, 50)
-                elif ch in BLANKS:      lexeme, state = accept(ch, 1001)
-                else:                   display_error(ch)
-
+                if ch == 'a': lexeme, state = go_to_state(ch, 30)
+                elif is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+                else: display_error(ch)
+            
             case 30:
-                if ch == "d":           lexeme, state = go_to_state(ch, 40)
-                elif is_alphanum(ch):   lexeme, state = go_to_state(ch, 50)
-                elif ch in BLANKS:      lexeme, state = accept(ch, 1001)
-                else:                   display_error(ch)
-
+                if ch == 'd': lexeme, state = go_to_state(ch, 40)
+                elif is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+                else: display_error(ch)
+            
             case 40:
-                if is_alphanum(ch):     lexeme, state = go_to_state(ch, 50)
-                if ch in BLANKS:        lexeme, state = accept(ch, 1002)
-                else:                   display_error(ch)             
+                if ch == 'd': lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1002)
+                else: display_error(ch)
+
+            case 50:
+                if is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+            
+            case 60:
+                if ch == 'r': lexeme, state = go_to_state(ch, 70)
+                elif is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+                else: display_error(ch)
+            
+            case 70:
+                if ch == 'i': lexeme, state = go_to_state(ch, 80)
+                elif is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+                else: display_error(ch)
+            
+            case 80:
+                if ch == 't': lexeme, state = go_to_state(ch, 90)
+                elif is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+                else: display_error(ch)
+            
+            case 90:
+                if ch == 'e': lexeme, state = go_to_state(ch, 100)
+                elif is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1001)
+                else: display_error(ch)
+
+            case 100:
+                if is_alphanum(ch): lexeme, state = go_to_state(ch, 50)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1003)
+                else: display_error(ch)
+
+            case 110:
+                if is_digit(ch): lexeme, state = go_to_state(ch, 110) 
+                elif ch == '.': lexeme, state = go_to_state(ch, 120)
+                elif ch in BLANKS: lexeme, state = accept(ch, 1004)
+                else: display_error(ch)
+
+            case 120:
+                if is_digit(ch): lexeme, state = go_to_state(ch, 120) 
+                elif ch in BLANKS: lexeme, state = accept(ch, 1004)
+                else: display_error(ch)
+            
+            case 130:
+                if is_digit(ch): lexeme, state = go_to_state(ch, 130) 
+                else: display_error(ch)
+
+            case 140:
+                if is_digit(ch): lexeme, state = go_to_state(ch, 140) 
+                elif ch in BLANKS: lexeme, state = accept(ch, 1004)
+                else: display_error(ch)
+
+            case 150:
+                if ch == ':': lexeme, state = go_to_state(ch, 1005)
+                else: display_error(ch)
+            
